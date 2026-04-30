134d4a873942aea06a8eef15b90278e78416e32b
Pablo Luna-Rodriguez <lunarodriguezpablo@gmail.com>
2026-04-30 08:14:27 -0500

    Update summary_statistics_bam_alignment.R

--- summary_statistics_bam_alignment.R
+++ summary_statistics_bam_alignment.R
@@ -41,20 +41,22 @@ data_long <- data %>%
 annotation_text <- paste0("QC-passed mapped reads: ", pmr1, "\n",
                           "QC-failed mapped reads: ", pmr2)
 
-p1 <- ggplot(data_long, aes(y = values, x = '', fill = type)) +
+p1 <- ggplot(data_long, aes(x = values, y = '', fill = type)) +
   geom_col(position = "dodge", color='black') +
-  scale_fill_manual(values = c("#d6ad2aff", "#642e8cff"))+
-  labs(x = '',
-       y = 'Read count',
+  geom_text(aes(label = values), position = position_dodge(width = 0.9), hjust = -0.1, size = 3) +
+  scale_fill_manual(values = c("#d6ad2aff", "#642e8cff")) +
+  scale_x_continuous(limits = c(0, NA), expand = expansion(mult = c(0, 0.2))) +
+  labs(y = '',
+       x = 'Read count',
        fill = '',
        title='Read Alignment Categories',
-       tag = annotation_text) +          
+       subtitle=paste0("QC-passed mapped reads: ", pmr1, "\n",
+                       "QC-failed mapped reads: ", pmr2)) +          
   theme_bw() +
-  facet_wrap(~ V4, scales = "free_y") +
+  facet_wrap(~ V4, ncol = 1, scales = "free_x") +
   theme(legend.position = 'top',
-        axis.ticks.x = element_blank(),
-        plot.tag.position = c(0.7, 0.2),
-        plot.tag = element_text(size = 10, hjust = 0.5))
+        axis.ticks.y = element_blank(),
+        panel.grid = element_blank())
 
 # 2. Histogram of quality
 mapq_data <- read.delim(mapq_file, header = FALSE)
@@ -66,20 +68,62 @@ p2 <- ggplot(mapq_data, aes(x = V1, y = V2)) +
   geom_col(color = 'black', fill = '#b81d1bff') +
   labs(x = "MQ", y = "Reads",
        title = paste0('Mapping Quality (mean: ', round(wm, 2), ')')) +
-  theme_bw()
+  theme_bw()+
+  theme(panel.grid = element_blank())
 
 # 3. Coverage per chromosome
 data3 <- read.delim(mosdepth_dist_file, header=F)
+
+data3_1<-data3[data3$V3 !=0,]
+data3_1$V4<-data3_1$V3*100
+
+data3_1$V2<-as.numeric(data3_1$V2)
+data3_1$V1<-as.factor(data3_1$V1)
+
+#Up to 24 chromosomes
+custom_colors <- c(
+  "dodgerblue2", "#E31A1C", 
+  "green4",
+  "#6A3D9A", 
+  "#FF7F00", 
+  "black", "gold1",
+  "skyblue2", "#FB9A99", 
+  "palegreen2",
+  "#CAB2D6", 
+  "gray70", "khaki2",
+  "maroon", "orchid1", "deeppink1", "blue1", "steelblue4",
+  "darkturquoise", "green1", "yellow4", "yellow3",
+  "darkorange4", "brown"
+)
+
+p6 <- ggplot(data3_1, aes(x = V2, y = V4, color = V1)) +
+  geom_line(linewidth=1) +
+  scale_color_manual(values = custom_colors) +
+  scale_x_continuous(expand = c(0, 0), limits = c(0, NA)) +
+  scale_y_continuous(expand = c(0, 0), limits = c(0, NA)) +
+  labs(color = "",
+       title='Depth distribution',
+       y = "Breadth of coverage (%)",
+       x = "Depth") +
+  guides(color = guide_legend(ncol = 3)) +
+  theme_bw() +
+  theme(panel.grid = element_blank(),
+        legend.position = c(1, 1.05),
+        legend.justification = c(1, 1),
+        legend.background = element_blank())
+
 data3 <- data3[data3$V2 == 1, ]
 data3$V4 <- data3$V3*100
 data3$V1 <- as.factor(data3$V1)
 
-p3 <- ggplot(data3, aes(x = V1,y=V4)) +
-  geom_col(color='black', fill='#58bb9dff') +
-  labs(x = "", y = "Covered by at least 1 read (%)",
+p3<-ggplot(data3, aes(y = V1,x=V4)) +
+  geom_col(color='black',,fill='#58bb9dff') +
+  labs(y = "", x = "Covered by at least 1 read (%)",
        title = 'Breadth of Coverage') +
   theme_bw()+
-  theme(axis.text.x = element_text(angle=45,hjust = 1))
+  geom_text(aes(label = V4), position = position_dodge(width = 0.9), hjust = -0.1, size = 3) +
+  scale_x_continuous(limits = c(0, 100), expand = expansion(mult = c(0, 0.1))) +
+  theme(panel.grid = element_blank())
 
 # 4. Proportion of genome at coverage / Depth chromosomes
 data4 <- read.delim(mosdepth_regions_file, header=F)
@@ -90,8 +134,9 @@ p4 <- ggplot(data4, aes(x = V2, y = V4)) +
   geom_area(fill = "darkblue") +
   facet_wrap(~ V1, ncol = 1, scales = "free", strip.position = "right") +
   scale_x_continuous(limits = c(0, NA), expand = c(0, 0)) +
-  labs(x = "Genomic Position", y = "Depth") +
-  theme_bw()
+  labs(x = "Genomic Position", y = "Depth")  +
+  theme_bw()+
+  theme(panel.grid = element_blank())
 
 mean_depth_chr <- data4 %>%
   mutate(window_size = V3 - V2) %>%
@@ -103,15 +148,20 @@ mean_depth_chr <- data4 %>%
     mean_depth = weighted.mean(data4$V4, w = data4$V3 - data4$V2)
   )
 
-p5 <- ggplot(mean_depth_chr, aes(x = V1,y=mean_depth)) +
-  geom_col(color='black', fill='#b35623ff') +
-  labs(x = "", y = "Mean depth",
+p5<-ggplot(mean_depth_chr, aes(y = V1,x=mean_depth)) +
+  geom_col(color='black',,fill='#b35623ff') +
+  labs(y = "", x = "Mean depth",
        title = 'Depth of Aligned Reads') +
   theme_bw()+
-  theme(axis.text.x = element_text(angle=45,hjust = 1))
+  scale_x_continuous(limits = c(0,NA), expand = expansion(mult = c(0, 0.2))) +
+  geom_text(aes(label = round(mean_depth,2)), position = position_dodge(width = 0.9), hjust = -0.1, size = 3) +
+  theme(panel.grid = element_blank())
 
 # Save plots
-plot1 <- arrangeGrob(p1,p2,p3,p5,ncol=2)
+plot1 <- arrangeGrob(p1, p2, p3, p5, p6,
+                     layout_matrix = rbind(c(1, 3),
+                                           c(1, 4),
+                                           c(2, 5)))
 
 out_file_1 <- paste0(output_prefix, "_alignment_summary_1.png")
 out_file_2 <- paste0(output_prefix, "_alignment_summary_2.png")
